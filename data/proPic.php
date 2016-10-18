550faf22500299e062bb75e3327e2375f2819d1a
mringWang <624997594@qq.com>
2016-10-18 19:50:59 +0800

    Update proPic.php

--- data/proPic.php
+++ data/proPic.php
@@ -4,9 +4,8 @@ header("Content-Type:application/json;charset=utf-8");
 $conn=mysqli_connect('127.0.0.1','root','','dahua',3306);
 $sql="SET NAMES UTF8";
 mysqli_query($conn,$sql);
-$count=4;
-$start=0;
-$sql="SELECT * FROM pro_advpic LIMIT $start,$count";
+
+$sql="SELECT * FROM pro_advpic";
 $result=mysqli_query($conn,$sql);
 $output=[];
 
